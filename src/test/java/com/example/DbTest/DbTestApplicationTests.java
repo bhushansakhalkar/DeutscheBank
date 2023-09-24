d7228601e72135bf4177c6d9f1bb1edb3fe1922c
bhushansakhalkar <bhushan.sakhalkar@gmail.com>
2023-09-24 23:46:07 +0200

    Test to check the logic
    + Added a test to check if the logic is correct

--- src/test/java/com/example/DbTest/DbTestApplicationTests.java
+++ src/test/java/com/example/DbTest/DbTestApplicationTests.java
@@ -1,13 +1,48 @@
-// package com.example.DbTest;
+package com.example.DbTest;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// @SpringBootTest
-// class DbTestApplicationTests {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// }
+import com.example.DbTest.Service.Signalhandler;
+
+@SpringBootTest
+class DbTestApplicationTests {
+
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+	@Test
+	public void testSignalHandlerForCorrectInput() {
+        Signalhandler signalhandler = new Signalhandler();
+        signalhandler.handleSignal(2);
+        assertEquals("reverse\r\n" + //
+                "setAlgoParam 1,80\r\n" + //
+                "submitToMarket\r\n" + //
+                "doAlgo\r\n" + //
+                        "",outContent.toString());
+	
+    }
+
+}
